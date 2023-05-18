16e6331fdd31323c0b84c38069a76f73f518ba43
LynovK <lynov.nk@phystech.edu>
2023-05-18 09:53:17 +0300

    !

---
+++ cmake-build-debug/CMakeFiles/LynovSlau.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LynovSlau.dir/CSR.cpp.obj"
+  "LynovSlau.exe"
+  "LynovSlau.exe.manifest"
+  "LynovSlau.lib"
+  "LynovSlau.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LynovSlau.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
